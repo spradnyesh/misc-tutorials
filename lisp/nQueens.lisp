9f86ef4d192fd65fa0239075c3f908f970252c20
Pradnyesh Sawant <me@spradnyesh.org>
2011-11-20 20:47:47 +0530

    global *Q-positions* => local queens

--- lisp/nQueens.lisp
+++ lisp/nQueens.lisp
@@ -1,23 +1,19 @@
-;; define the array that holds the positions of the queens on the board
-(defvar *Q-positions* (make-array 4))
-;; initialize such that all the Qs are in the 1st column initially
-(defun init-Q-positions ()
-  (dotimes (i (length *Q-positions*))
-    (setf (aref *Q-positions* i) (list i 0))))
-
 (defun row (queen)
   (first queen))
 (defun column (queen)
   (second queen))
-(defun kth-queen (k)
-  (aref *Q-positions* k))
+(defun kth-queen (queens k)
+  (aref queens k))
 
-(defun move-queen (k position)
-  (setf (aref *Q-positions* k) (list k position)))
-(defun move-queen-forward (k)
-  (move-queen k (1+ (column (kth-queen k)))))
-(defun move-queen-to-1st-column (k)
-  (move-queen k 1))
+(defun init-Q-positions (queens)
+  (dotimes (i (length queens))
+    (setf (aref queens i) (list i 0))))
+(defun move-queen (queens k position)
+  (setf (aref queens k) (list k position)))
+(defun move-queen-forward (queens k)
+  (move-queen queens k (1+ (column (kth-queen queens k)))))
+(defun move-queen-to-1st-column (queens k)
+  (move-queen queens k 0))
 
 (defun same-column? (i j)
   (when (= (column i)
@@ -32,21 +28,53 @@
 ;;     2. one of the previous Q is in the same column
 ;;     3. one of the previous Q is in the same diagonal
 ;; else: nil/false (Q isn't under attack)
-(defun under-attack? (k)
+(defun under-attack? (queens k)
   (dotimes (i k)
-    (cond ((same-column? (kth-queen i) (kth-queen k)) (return t))
-          ((same-diagonal? (kth-queen i)(kth-queen k)) (return t)))))
-(defun build-board (n)
-  (init-Q-positions)
+    (cond ((same-column? (kth-queen queens i) (kth-queen queens k)) (return t))
+          ((same-diagonal? (kth-queen queens i)(kth-queen queens k)) (return t)))))
+(defun retrace (queens i n)
+  (if (< (column (kth-queen queens i))
+         (1- n))
+      (move-queen-forward queens i)          ; there is place to move forward
+      (progn              ; no place to move forward, retrace back 1 Q
+        (move-queen-to-1st-column queens i)
+        (setf i (1- i))
+        (retrace queens i n))))
+(defun build-board (n &aux (queens (make-array n)))
+  (init-Q-positions queens)
   (do ((i 1)) ; start from the 2nd Q
-      ((= i n) *Q-positions*)
-    (if (under-attack? i)
+      ((= i n) queens)
+    (if (under-attack? queens i)
          ; Q is under attack
-        (if (< (column (kth-queen i))
-               n)
-            (move-queen-forward i) ; there is place to move forward
+        (if (< (column (kth-queen queens i))
+               (1- n))
+            (move-queen-forward queens i) ; there is place to move forward
             (progn ; no place to move forward, retrace back 1 Q
-              (move-queen-to-1st-column i)
-              (setf i (1- i))))
+              (move-queen-to-1st-column queens i)
+              (setf i (1- i))
+              (retrace queens i n)))
         ; Q isn't under attack, move to the next Q
         (setf i (1+ i)))))
+
+
+;;;; tests
+(stefil:in-root-suite)
+(stefil:defsuite* test-all)
+(stefil:defsuite test-functions)
+(stefil:defsuite test-problems)
+
+(stefil:in-suite test-functions)
+
+(stefil:deftest test-same-column-ok ()
+  (stefil:is (eql t (same-column? '(0 0) '(1 0)))))
+(stefil:deftest test-same-column-not-ok ()
+  (stefil:is (not (eql t (same-column? '(0 0) '(1 1))))))
+(stefil:deftest test-same-diagonal-ok ()
+  (stefil:is (eql t (same-diagonal? '(0 0) '(1 1)))))
+(stefil:deftest test-same-diagonal-not-ok ()
+  (stefil:is (not (eql t (same-diagonal? '(0 0) '(1 2))))))
+
+(stefil:in-suite test-problems)
+
+#|(stefil:deftest test-build-board-ok ()
+  (stefil:is (equal '((0 1) (1 3) (2 0) (3 2)) (build-board 4))))|#
