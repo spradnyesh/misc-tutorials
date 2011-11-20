cab9232003733d7c20e34eba2838862b46a6f108
Pradnyesh Sawant <me@spradnyesh.org>
2011-11-20 22:38:53 +0530

    sfrd @ Sun Nov 20 22:38:53 IST 2011

--- lisp/nQueens.lisp
+++ lisp/nQueens.lisp
@@ -3,19 +3,28 @@
 (defun column (queen)
   (second queen))
 
-(defun build-board (n &aux (queens (make-array n)))
+(defmacro move-queen (i)
+  `(if (< (column (kth-queen ,i)) (1- n)) ; is there place to move forward?
+       (move-queen-forward ,i)            ; yes
+       (progn                             ; no, retrace back 1 Q
+         (move-queen-to-1st-column ,i)
+         (setf ,i (1- ,i))
+         (retrace ,i n))))
+
+(defun build-all-boards (n &aux (queens (make-array n)) (result nil))
   (defun init-Q-positions ()
     (dotimes (i (length queens))
       (setf (aref queens i) (list i 0))))
   (defun kth-queen (k)
     (aref queens k))
 
-  (defun move-queen (k position)
+  (defun move-queen-to-position (k position)
     (setf (aref queens k) (list k position)))
   (defun move-queen-forward (k)
-    (move-queen k (1+ (column (kth-queen k)))))
+    (move-queen-to-position k (1+ (column (kth-queen k)))))
   (defun move-queen-to-1st-column (k)
-    (move-queen k 0))
+    (move-queen-to-position k 0))
+
 
   ;; is the k'th Q under attack from the previous ones?
   ;; k'th Q is under attack if:
@@ -38,7 +47,7 @@
   (defun retrace (i n)
     (if (< (column (kth-queen i))
            (1- n))
-        (move-queen-forward i) ; there is place to move forward
+        (move-queen-forward i)        ; there is place to move forward
         (progn            ; no place to move forward, retrace back 1 Q
           (move-queen-to-1st-column i)
           (setf i (1- i))
@@ -46,18 +55,22 @@
 
   (init-Q-positions)
   (do ((i 1))                           ; start from the 2nd Q
-      ((= i n) queens)
+      ((>= (column (kth-queen 0)) (/ n 2)) result)
+    (print (list i queens result))
     (if (under-attack? i)
-                                        ; Q is under attack
-        (if (< (column (kth-queen i))
-               (1- n))
-            (move-queen-forward i) ; there is place to move forward
-            (progn        ; no place to move forward, retrace back 1 Q
-              (move-queen-to-1st-column i)
-              (setf i (1- i))
-              (retrace i n)))
-                                        ; Q isn't under attack, move to the next Q
-        (setf i (1+ i)))))
+        ; Q is under attack
+        (move-queen i)
+        ; Q isn't under attack, so if this is the last Q
+        ;     then append current board-position to result and continue
+        ;     else move to the next Q
+        (if (= i (1- n))
+            (progn
+              (print "if")
+              (setf result (append result (list queens)))
+              (move-queen i))
+            (progn
+              (print "else")
+              (setf i (1+ i)))))))
 
 
 ;;;; tests
