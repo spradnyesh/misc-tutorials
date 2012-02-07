2f0239fe05f860ac5613210535508dc78ec515a0
Pradnyesh Sawant <me@spradnyesh.org>
2012-02-07 17:15:07 +0530

    sfrd @ Tue Feb  7 17:15:07 IST 2012

---
+++ js/twiki.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Pradnyesh's script for removing the left panel of twiki.cyc
+// @namespace http://www.spradnyesh.org
+// @match http://twiki.corp.yahoo.com/view/*
+// @description remove '#patternLeftBar'
+// ==/UserScript==
+
+var wrap = document.getElementById('patternFloatWrap');
+wrap.removeChild(document.getElementById('patternLeftBar'));
+
+var outer = document.getElementById('patternOuter');
+outer.style.marginLeft="0";
