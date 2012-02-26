85b909ea3bfb66826f43621d1fbad3b5f8418ab4
Pradnyesh Sawant <me@spradnyesh.org>
2012-02-26 21:33:31 +0530

    sfrd @ Sun Feb 26 21:33:31 IST 2012

---
+++ js/mangareader.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Pradnyesh's script for mangareader image positioning
+// @namespace http://www.spradnyesh.org
+// @match http://www.mangareader.net/*/*/chapter-*.html
+// @description scrollIntoView
+// ==/UserScript==
+
+document.getElementById('imgholder').scrollIntoView();
