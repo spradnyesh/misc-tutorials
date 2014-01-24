645061cb09a67808b203e9a8422571a1fe8eeb54
Pradnyesh Sawant <me@spradnyesh.org>
2014-01-24 16:01:51 +0900

    clojure: project-euler: 3rd problem (90 mins completed w/ help from stackoverflow)

--- clojure/project-euler/src/project_euler/prob003.clj
+++ clojure/project-euler/src/project_euler/prob003.clj
@@ -1,28 +1,14 @@
 (ns project-euler.prob003
   (:require [clojure.math.numeric-tower :as math]))
 
-(defn is-prime [n]
-  (if (<= n 3)
-    true
-    (do
-      (for [i (range 2 (math/floor (math/sqrt n)))
-            :while (= 0 (rem n i))]
-        true)
-      false)))
-
-(defn next-prime
-  ([min] (next-prime min 999999))
-  ([min max]
-     (first (for [i (range (inc min) max)
-                  :when (= true (is-prime i))]
-              i))))
-
-(defn ith-prime [i]
-  (if (= i 0)
-    1
-    (next-prime (ith-prime (dec i)))))
-
-(def ith-prime (memoize ith-prime))
-
 (defn prime-factors [n]
-  )
+  (let [num (atom n)
+        factor (atom 2)
+        last-factor (atom 1)]
+    (while (> @num 1)
+      (when (= 0 (rem @num @factor))
+        (swap! last-factor #(do %2) @factor)
+        (while (= 0 (rem @num @factor))
+          (swap! num / @factor)))
+      (swap! factor inc))
+    @last-factor))
--- clojure/project-euler/test/project_euler/prob003_test.clj
+++ clojure/project-euler/test/project_euler/prob003_test.clj
@@ -2,12 +2,4 @@
   (:use expectations
         project-euler.prob003))
 
-;; is-prime should return true/false based upon whether input is prime or not
-(expect true (is-prime 3))
-(expect false (is-prime 4))
-
-;; next-prime should return prime number between the input
-(expect 13 (next-prime 12 100))
-
-;; prime-factors should return all prime factors
-(expect [5 7 13 29] (prime-factors 13195))
+(expect 6857 (prime-factors 600851475143))
