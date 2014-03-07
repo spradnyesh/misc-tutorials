9702d77f13231b1c5ee6d5f0fa8ae8f13db2b24c
Pradnyesh Sawant <me@spradnyesh.org>
2014-03-07 17:43:32 +0900

    sfrd before a fresh start

--- codechef/src/drghts.lisp
+++ codechef/src/drghts.lisp
@@ -1,31 +1,90 @@
+; http://www.codechef.com/problems/DRGHTS
 (in-package :codechef)
 
-; http://www.codechef.com/problems/DRGHTS
+(defun graph-to-forest (n passages)
+  (let ((forest nil))
+    (dotimes (i n)
+      (push (list nil) forest))
+    (dolist (p passages)
+      (push (second p) (nth (1- (first p)) forest)))
+    (dotimes (i n)
+      (setf (nth i forest)
+            (cons (1+ i) (list (remove-if #'null (nth i forest))))))
+    (remove-if #'null forest :key #'second)))
 
-(defun is-drght? (path window-list)
-  (let ((src (first path))
-        (dest (second path)))
-    (when (and (not (zerop (* src (nth (1- src) window-list))))
-               (not (zerop (* dest (nth (1- dest) window-list)))))
-      t)))
-
-(defun furik (list)
+(defun flatten (list)
   (let ((rslt nil))
     (dolist (l list)
-      (setf rslt (adjoin (first l) rslt))
-      (setf rslt (adjoin (second l) rslt)))
-    (length rslt)))
+      (if (atom l)
+          (push l rslt)
+          (dolist (f (flatten l))
+            (push f rslt))))
+    rslt))
 
-(defun rubik (list)
-  (length list))
+(defun depth-first-helper (node forest)
+  (let ((children (second (assoc node forest))))
+    (when children
+      (append children (loop for child in children
+                          collecting (depth-first-helper child forest))))))
 
-(defun drghts (n m window-list passages)
-  (declare (ignore n m))
+(defun depth-first (forest)
+  (let ((expanded-forest nil))
+    (dolist (tree forest)
+      (let ((root (first tree)))
+        (push (list root
+                    (remove-duplicates (remove-if #'null (flatten (depth-first-helper root forest)))))
+              expanded-forest)))
+    expanded-forest))
+
+(defun remove-closed-window-rooms (window-list forest)
+  ;; remove complete node itself
+  (dotimes (i (length window-list))
+    (let ((w (nth i window-list)))
+      (when (zerop w)
+        (setf forest (remove-if #'(lambda (a)
+                                    (= (1+ i) (first a)))
+                                forest)))))
+  ;; remove child from node
+  (dolist (tree forest)
+    (dotimes (i (length window-list))
+      (let ((w (nth i window-list))
+            (root (first tree)))
+        (when (zerop w)
+          (setf forest (remove-if #'(lambda (a)
+                                      (= root a))
+                                  forest
+                                  :key #'first))
+          (push (list root
+                      (remove-if #'(lambda (a)
+                                     (= (1+ i) a))
+                                 (second tree)))
+                forest)))))
+  forest)
+
+(defun make-symmetric (passages)
   (let ((rslt nil))
     (dolist (p passages)
-      (when (is-drght? p window-list)
-        (push p rslt)))
-    (list (furik rslt) (rubik rslt))))
+      (push p rslt)
+      (push (reverse p) rslt))
+    rslt))
+
+(defun furik (forest)
+  (reduce #'+ forest :key #'(lambda (a) (length (second a)))))
+
+(defun rubik (forest)
+  (let ((rslt nil))
+    (dolist (tree forest)
+      (push (first tree) rslt)
+      (dolist (node (second tree))
+        (push node rslt)))
+    (length (remove-duplicates rslt))))
+
+(defun drghts (n m window-list passages)
+  (declare (ignore m))
+  (let ((forest  (depth-first (graph-to-forest n (make-symmetric passages)))
+          (remove-closed-window-rooms window-list
+                                      )))
+    (list (furik forest) (rubik forest))))
 
 (defun main ()
   (let ((n (read))
--- codechef/tests/drghts.lisp
+++ codechef/tests/drghts.lisp
@@ -1,43 +1,82 @@
 (in-package :codechef-tests)
 
+; (5 (0 0 0 1 1 1) ((6 2) (1 4) (4 2) (4 5) (1 5)))
+
 (def-suite :drghts :in :codechef)
 (in-suite :drghts)
 
-(test is-drght?-nil
-  (is-false (codechef::is-drght? '(8 9) '(1 1 1 1 1 0 0 1 0 1))))
-(test is-drght?-t
-  (is-true (codechef::is-drght? '(1 10) '(1 1 1 1 1 0 0 1 0 1))))
-(test furik-1
-  (is (equal (codechef::furik '((5 4)
-                                (2 3)))
-             4)))
-(test furik-2
-  (is (equal (codechef::furik '((5 4)
-                                (4 3)))
-             3)))
+(test make-symmetric
+  (is (equal (codechef::make-symmetric '((1 2) (1 6) (1 5) (2 4) (3 4)))
+             '((4 3) (3 4) (4 2) (2 4) (5 1) (1 5) (6 1) (1 6) (2 1) (1 2)))))
+(test graph-to-forest-1
+  (is (equal (codechef::graph-to-forest 6 '((4 3) (3 4) (4 2) (2 4) (5 1) (1 5) (6 1) (1 6) (2 1) (1 2)))
+             '((1 (5 6 2))
+               (2 (4))
+               (3 (4))))))
+(test graph-to-forest-2
+  (is (equal (codechef::graph-to-forest 6 '((1 5) (2 5) (3 6) (4 5) (5 6)))
+             '((1 (5))
+               (2 (5))
+               (3 (6))
+               (4 (5))
+               (5 (6))))))
+(test flatten
+  (is (equal (codechef::flatten '(2 5 6 (4 (3 NIL)) NIL NIL))
+             '(NIL NIL 4 NIL 3 6 5 2))))
+(test depth-first-helper
+  (is (equal (codechef::depth-first-helper 1 '((1 (2 5 6))
+                                               (2 (4))
+                                               (4 (3))))
+             '(2 5 6 (4 (3 NIL)) NIL NIL))))
+(test depth-first
+  (is (equal (codechef::depth-first '((1 (2 5 6))
+                                      (2 (4))
+                                      (4 (3))))
+             '((4 (3))
+               (2 (3 4))
+               (1 (4 3 6 5 2))))))
+(test remove-closed-window-rooms
+  (is (equal (codechef::remove-closed-window-rooms '(1 1 1 1 1 0) '((4 (3))
+                                                                    (2 (3 4))
+                                                                    (1 (4 3 6 5 2))))
+             '((1 (4 3 5 2))
+               (2 (3 4))
+               (4 (3))))))
+(test furik
+  (is (equal (codechef::furik '((3 (5 4))
+                                (4 (5))
+                                (2 (4 3 5))
+                                (1 (4 3 5 2))))
+             10)))
 (test rubik
-  (is (equal (codechef::rubik '((5 4)
-                                (2 3)))
-             2)))
+  (is (equal (codechef::rubik '((3 (5 4))
+                                (4 (5))
+                                (2 (4 3 5))
+                                (1 (4 3 5 2))))
+             5)))
 (test drghts-1
-  (is (equal (codechef::drghts 6 5 '(1 1 1 1 1 0)
-                               '((1 2) (1 6) (1 5) (2 4) (4 3)))
-             '(5 4))))
+  (is (equal (codechef::drghts 6 5 '(1 1 1 1 1 0) '((1 2)
+                                                    (1 6)
+                                                    (1 5)
+                                                    (2 4)
+                                                    (4 3)))
+             '(10 5))))
 (test drghts-2
   (is (equal (codechef::drghts 2 1 '(1 0) '((1 2)))
              '(0 0))))
 (test drghts-3
   (is (equal (codechef::drghts 10 8 '(1 1 1 1 1 0 0 1 0 1)
                                '((8 9) (6 7) (9 5) (8 1) (6 5) (10 1) (6 9) (2 10)))
-             '(4 3))))
+             '(6 5))))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; data to test performance
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun remove-cycles (passages)
   (let ((rslt nil))
     (dolist (p passages)
-      (unless (or (= (first p) (second p))
-                  (member (reverse p) rslt :test #'equal))
+      (unless (member (reverse p) rslt :test #'equal)
         (push p rslt)))
     rslt))
 (defun generate-perf-data (n)
